9f7eb8ffae7b59ce8102d75675af85579140d16e
Willian Duarte <willianm.cduarte@gmail.com>
2019-06-22 20:40:20 -0300

    :smile: Created Header

---
+++ ten-products-react/src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="hero is-info">
+            <div className="hero-body">
+                <div className="container">
+                    <h1 className="title">Products</h1>
+                    <h2 className="subtitle">Here, I sell products...</h2>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
